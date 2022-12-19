e50675e50c99eddf8e4a94ec5661081f26c10752
Uchpochmax <118854692+Uchpochmax@users.noreply.github.com>
2022-12-20 02:49:59 +0300

    Добавлен таймер, анимация, победа/поражение

--- js/start-game.js
+++ js/start-game.js
@@ -16,22 +16,70 @@ export const startGame = (difficult) => {
     const cardsArray = createIconArray(difficult);
     const duplicatedCardsIcons = duplicateArray(cardsArray);
     const restartBtn = document.createElement('button');
+    const gameTimerHeader = document.createElement('h2');
+    const timerCount = document.createElement('div');
+    let interval;
+
+    // Создание таймера
+    const gameTimer = (time) => {
+        // Создание переменных по именам классов 
+        timerCount.classList.add('game-timer');
+
+        // Функция, которая уменьшает значение таймера
+        function timerStart() {
+            if (timerCount.innerHTML > 0) {
+                timerCount.innerHTML--;
+            }
+            if (timerCount.innerHTML <= 10) {
+                timerCount.classList.remove(`last-${Number(timerCount.innerHTML)+1}-seconds`);
+                timerCount.classList.add(`last-${timerCount.innerHTML}-seconds`);
+            }
+            if (timerCount.innerHTML == 0) {
+                clearInterval(interval);
+                gameTable.remove();
+                timerCount.remove();
+                gameTimerHeader.innerHTML = 'Попробуйте снова!';
+                const loseGame = document.createElement('div');
+                loseGame.innerHTML = '';
+                gameTimerHeader.before(loseGame);
+                loseGame.classList.add('game-over');
+                setTimeout(() => {
+                    loseGame.innerHTML = 'Game Over';
+                    loseGame.classList.add('move');
+                }, 100);
+            };
+        };
+
+        // Функция, задающая интервал
+        function countdown(time) {
+            timerCount.innerHTML = time;
+            clearInterval(interval);
+            interval = setInterval(timerStart, 1000);
+        }
+
+        countdown(time);
+
+    }
 
     gameSection.innerHTML = '';
     restartBtn.textContent = 'Рестарт';
+    gameTimerHeader.textContent = 'У вас есть ровно 60 секунд:';
     gameTable.classList.add('game-table');
-    restartBtn.classList.add('restart-btn')
+    restartBtn.classList.add('restart-btn');
+    gameTimerHeader.classList.add('header-timer');
 
     shuffle(duplicatedCardsIcons);
+    gameTimer(60);
 
     // для каждого элемента дублированного массива создается div с классом .game-card
     // с двумя элементами i
     duplicatedCardsIcons.forEach(element => gameTable.append(createGameCard('question-circle', element)));
 
     // добавление в секцию игры игрового стола и кнопки рестарт
-    gameSection.append(gameTable, restartBtn);
+    gameSection.append(gameTable, gameTimerHeader, timerCount, restartBtn);
 
-    restartBtn.addEventListener('click', createGameMenu)
+    restartBtn.addEventListener('click', createGameMenu);
+
 
     // константа с элементами div и классом .game-card
     const cards = document.querySelectorAll('.game-card');
@@ -80,6 +128,14 @@ export const startGame = (difficult) => {
 
             if (Array.from(cards).every(card => card.className.includes('flip'))) {
                 document.querySelector('.salut').innerHTML = confetti;
+                clearInterval(interval);
+                gameTimerHeader.innerHTML = 'Это было просто потрясающе! Вы уложились всего за';
+                const winTime = 60 - Number(timerCount.innerHTML);
+                timerCount.innerHTML = winTime;
+                const winGame = document.createElement('h2');
+                winGame.innerHTML = 'секунд. Юхууу!';
+                restartBtn.before(winGame);
+                winGame.classList.add('header-timer');
             }
         }
     }))
